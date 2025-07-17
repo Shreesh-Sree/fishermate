8d2d8e11978e8b3aacad36bb50bcce3846c3d346
Sreesanth R <shreesh.exe22@gmail.com>
2025-07-17 23:15:10 +0530

    BUG 005

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import GoogleMapCard from "@/components/GoogleMapCard";
+import { MapCard } from "@/components/MapCard";
 import { Header } from "@/components/Header";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function MapPage() {
   return (
     <ProtectedRoute>
-      <div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-50 via-white to-cyan-50">
+      <div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-50 via-white to-cyan-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900">
         <Header />
         <main className="flex-1 container mx-auto p-4 md:p-8">
           <div className="h-[80vh]">
-            <GoogleMapCard />
+            <MapCard />
           </div>
         </main>
       </div>
