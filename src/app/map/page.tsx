0f188202e7c32bd08b03af57e7a72ab6a8ab3296
Sreesanth R <shreesh.exe22@gmail.com>
2025-07-18 09:28:29 +0530

    FINAL PUSH 003

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -9,8 +9,8 @@ export default function MapPage() {
     <ProtectedRoute>
       <div className="flex flex-col min-h-screen bg-background gradient-bg">
         <Header />
-        <main className="flex-1 container mx-auto p-2 sm:p-4 md:p-8 pt-20 sm:pt-24">
-          <div className="h-[calc(100vh-140px)] sm:h-[calc(80vh-80px)]">
+        <main className="flex-1 container mx-auto p-2 sm:p-4 md:p-8 pt-28 sm:pt-32">
+          <div className="h-[calc(100vh-180px)] sm:h-[calc(80vh-120px)]">
             <GoogleMapCard />
           </div>
         </main>
--- src/app/safety/page.tsx
+++ src/app/safety/page.tsx
@@ -9,7 +9,7 @@ export default function SafetyPage() {
     <ProtectedRoute>
       <div className="flex flex-col min-h-screen bg-background gradient-bg">
         <Header />
-        <main className="flex-1 container mx-auto p-2 sm:p-4 md:p-8 pt-20 sm:pt-24">
+        <main className="flex-1 container mx-auto p-2 sm:p-4 md:p-8 pt-28 sm:pt-32">
           <SafetyTips />
         </main>
       </div>
