aebc2d5310e5ca692712e30b0f3b8369b55ac5da
Sreesanth R <shreesh.exe22@gmail.com>
2025-07-19 09:16:23 +0530

    SECURITY PUSH

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCp7H35BmfdoCOvbvtqXssCrNf3mhRbqmc",
-  authDomain: "studio-1uekq.firebaseapp.com",
-  projectId: "studio-1uekq",
-  storageBucket: "studio-1uekq.firebasestorage.app",
-  messagingSenderId: "985191008807",
-  appId: "1:985191008807:web:aee41d452a6be210f31c02"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
